fcb50ce22dd523b7422214e1388b6eb12260b99e
Ashlirankin18 <ashlirankin18@gmail.com>
2018-12-18 11:15:57 -0500

    Initial Commit

---
+++ WrldNow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WrldNow
+//
+//  Created by Ashli Rankin on 12/18/18.
+//  Copyright © 2018 Ashli Rankin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
